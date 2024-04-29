cef1d05d5a33375f61b9ca49a091c3f67b11c847
Sukesh <sukesh2001.s2@gmail.com>
2024-04-29 21:32:00 +0530

    feat: Created  main landing page for portfolio Add a home section to the portfolio website's main landing page, including relevant images and text to enhance the visual appeal and communicate the purpose of the portfolio effectively.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { ExperienceSectionComponent } from './components/experience-section/experience-section.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeroSectionComponent,ExperienceSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/experience-section/experience-section.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experience-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './experience-section.component.html',
+  styleUrl: './experience-section.component.css'
+})
+export class ExperienceSectionComponent {
+
+}
